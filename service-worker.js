a2ea46887a294fbf41faa3e9ca2b5322dfb38934
Kim Jae Hyune <wogus4041@naver.com>
2019-09-05 15:17:11 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.59f5bac11a484be5dda3fdf607fe7ec6.js"
+  "/portfolio/precache-manifest.1099956ce33130e33c86ecba03a1b153.js"
 );
 
 self.addEventListener('message', (event) => {
