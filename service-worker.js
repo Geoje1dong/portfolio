510da07318e202a8b9b6fc5ca325afb806882961
Geoje1dong <wogus4041@naver.com>
2025-04-17 17:08:53 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8a0d8f2faf726ab5b1708d2dee1e55b0.js"
+  "/portfolio/precache-manifest.0e3ed3105de924d5563f92ad4fc85dad.js"
 );
 
 self.addEventListener('message', (event) => {
